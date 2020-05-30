6f67b379d9a533a8c3cbb21e2206cdcaff5c3bf3
Paul Silver <paul.silver@allscripts.com>
2020-05-30 11:26:50 -0400

    Initial commit

---
+++ AppConfigSettingsFramework/AppConfigSettingsFramework.h
@@ -0,0 +1,18 @@
+//
+//  AppConfigSettingsFramework.h
+//  AppConfigSettingsFramework
+//
+//  Created by Paul Silver on 5/30/20.
+//  Copyright © 2020 Paul Silver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppConfigSettingsFramework.
+FOUNDATION_EXPORT double AppConfigSettingsFrameworkVersionNumber;
+
+//! Project version string for AppConfigSettingsFramework.
+FOUNDATION_EXPORT const unsigned char AppConfigSettingsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppConfigSettingsFramework/PublicHeader.h>
+#import "ManagedAppConfigKeys.h"
